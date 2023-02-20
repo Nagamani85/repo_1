7eac327087a7bee81f59d1ff6ac5c64d1595c092
Nagamani85 <125533767+Nagamani85@users.noreply.github.com>
2023-02-20 15:49:08 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ streamlit.title('my parents new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omege3 and Blueberry Oatmeal')   
 streamlit.text('Kale Spinach and Rocket Smoothy')
-streamlit.text('Hard-Boiled Free Range Egg')
+streamlit.text('Hard-Boiled Free Range Egg ')
+
       
